a03c04e379c0a4e55ff3e8e5cb46ca6218802513
Goran Savic <goran.savic@incrowdsports.com>
2022-10-20 14:48:21 +0200

    additional examples

--- app.vue
+++ app.vue
@@ -2,6 +2,8 @@
 let username = ref('user')
 let userName2 = ref('user 2')
 let otherRef = ref('otherRef')
+let emailuser = ref('emailuser')
+let useremail = ref('useremail')
 let abc = ref('abc')
 let u = ref('u')
 let us = ref('us')
