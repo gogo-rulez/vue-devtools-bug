5701313bcce36ee0eff23e67eccec6b2f6ddf5b6
Goran Savic <goran.savic@incrowdsports.com>
2022-10-20 14:44:38 +0200

    setup

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup>
+let username = ref('user')
+let userName2 = ref('user 2')
+let otherRef = ref('otherRef')
+let abc = ref('abc')
+let u = ref('u')
+let us = ref('us')
+let use = ref('use')
+</script>
+
+<template>
+  <div>
+    <h1>username: {{ username }}</h1>
+  </div>
+</template>
